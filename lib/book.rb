a6477936c4d74e35540b9521e85be2e0938a89c5
Jennifer Prince <jennifergraceprince@gmail.com>
2020-05-28 15:04:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,8 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -9,10 +11,12 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+
   def genre=(genre)
     @genre = genre
-    GENRES << genre 
+    GENRES << genre
+    #or GENRES = @genre
   end
+
 end
 
